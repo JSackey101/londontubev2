70c98334d86453c79afc898525a608cd82245346
JSackey101 <jeffsackey101@gmail.com>
2023-12-07 10:44:41 +0000

    Added a Network.py file containing a Network class with 2 attributes: n_nodes and adjacency_matrix.

---
+++ londontube/Network.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class Network():
+    def __init__(self, n_nodes, adjacency_matrix):
+        self.n_nodes = n_nodes
+        self.adjacency_matrix = adjacency_matrix    
+    
+
